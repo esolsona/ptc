904714a314403d9fe5016b22a45473aa72ff6468
cmajo@kyndryl.com <cmajo@kyndryl.com>
2022-05-12 16:38:36 +0200

    adasd

--- objects/JENKINS/wrapper.sh
+++ objects/JENKINS/wrapper.sh
@@ -1,14 +1,10 @@
 #!/bin/sh 
 set -m
-#podman system migrate
-cd /tmp
-dockerd &
-
-alias docker="sudo docker"
-#chmod 666 /var/run/docker.sock
-
-#/sbinoecho "/tini -- /usr/local/bin/jenkins.sh
-echo "¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿"
-whoami
-echo "¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿¿"
-/opt/bitnami/scripts/jenkins/entrypoint.sh /opt/bitnami/scripts/jenkins/run.sh
+#dockerd &
+#sudo /var/jenkins_home/bin/dockerd &
+#/tmp/rootless.sh &
+#export XDG_RUNTIME_DIR=/var/jenkins_home/.docker/run
+export PATH=/var/jenkins_home/bin:$PATH
+export DOCKER_HOST=tcp://192.168.1.116:2375
+#/sbin/tini -- /usr/local/bin/jenkins.sh
+/usr/local/bin/jenkins.sh
