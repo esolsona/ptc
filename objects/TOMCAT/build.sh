95408a7b4235eb0109506ecfeee9851bbd73305c
cmajo@kyndryl.com <cmajo@kyndryl.com>
2022-05-17 11:43:31 +0200

    TOMCAT

--- objects/TOMCAT/build.sh
+++ objects/TOMCAT/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-echo "No executar el script!!"
-exit 0
+oc login -u developer -p developer https://api.crc.testing:6443 -n portic-devops || exit 1
 
 oc delete configmap tomcat-serverxml --ignore-not-found=true
 oc delete configmap tomcat-prometheus-config --ignore-not-found=true
@@ -10,5 +9,4 @@ oc create configmap tomcat-serverxml --from-file conf/server.xml
 oc create configmap tomcat-prometheus-config --from-file conf/tomcat.yml
 oc create configmap tomcat-setenv --from-file bin/setenv.sh
 
-
-
+oc apply -f deployment_tomcat.yaml 
