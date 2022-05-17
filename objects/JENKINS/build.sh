777a41f0d01945216d700b23b72a46e302a9966f
cmajo@kyndryl.com <cmajo@kyndryl.com>
2022-05-17 18:07:42 +0200

    build JENKINS

--- objects/JENKINS/build.sh
+++ objects/JENKINS/build.sh
@@ -2,5 +2,5 @@
 podman build . -t default-route-openshift-image-registry.apps-crc.testing/portic-devops/jenkins-portic:3.0 
 podman login default-route-openshift-image-registry.apps-crc.testing/portic-devops -u developer -p $(oc whoami -t) 
 podman push default-route-openshift-image-registry.apps-crc.testing/portic-devops/jenkins-portic:3.0
-oc set image deployment/jenkins2 container-0=default-route-openshift-image-registry.apps-crc.testing/portic-devops/jenkins-portic:3.0
+oc set image deployment/jenkins2 container-0=default-route-openshift-image-registry.apps-crc.testing/portic-devops/jenkins-portic:latest
 oc scale --replicas=1 deployment/jenkins2
