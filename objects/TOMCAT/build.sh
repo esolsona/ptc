6fc56f145a3538f8fece35b2119a93ba7b992d66
cmajo@kyndryl.com <cmajo@kyndryl.com>
2022-05-20 02:04:44 +0200

    build.sh added login

--- objects/TOMCAT/build.sh
+++ objects/TOMCAT/build.sh
@@ -19,7 +19,7 @@ docker login default-route-openshift-image-registry.apps-crc.testing/portic-devo
 echo "PUSH default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:${BUILD_NUMBER}"
 docker push default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:${BUILD_NUMBER}
 echo "SET IMAGE"
-oc set image --loglevel=1  deployment/tomcat8-portic tomcat8-portic=portic-devops/${IMAGE}:${BUILD_NUMBER}
+oc set image --loglevel=1  deployment/tomcat8-portic tomcat8-portic=image-registry.openshift-image-registry.svc:5000/portic-devops/${IMAGE}:${BUILD_NUMBER}
 #echo "TAG LATEST"
 #docker tag default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:${BUILD_NUMBER} default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:latest
 #echo "PUSH LATEST"
