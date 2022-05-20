2341eceb0aa501822da28f6ee715eab4f6520eed
cmajo@kyndryl.com <cmajo@kyndryl.com>
2022-05-20 08:37:25 +0200

    commit

--- objects/TOMCAT/build.sh
+++ objects/TOMCAT/build.sh
@@ -8,23 +8,25 @@ echo $PASS
 echo $BUILD_NUMBER
 echo "******************"
 
-wget https://carles.jfrog.io/artifactory/maven-libs-release/io/happycoding/hello-world-maven/1/hello-world-maven-1.war --user ${ART_USER} --password ${ART_PASSWD} || { echo "No se ha podido descargar el war del artifactory" && exit 1; }
 cd $(dirname $0)
+
+#Agafem artefactes 
+wget https://carles.jfrog.io/artifactory/maven-libs-release/io/happycoding/hello-world-maven/1/hello-world-maven-1.war --user ${ART_USER} --password ${ART_PASSWD} -O war/hello-world-maven-1.war || { echo "No se ha podido descargar el war del artifactory" && exit 1; }
+
 oc login -u developer -p developer https://api.crc.testing:6443 -n portic-devops --insecure-skip-tls-verify || exit 1
 
 echo "Build ${IMAGE}:${BUILD_NUMBER}"
 docker build . -t default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:${BUILD_NUMBER}
+docker tag default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:${BUILD_NUMBER} default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:latest
+
 echo "LOGIN REPO:"
 docker login default-route-openshift-image-registry.apps-crc.testing/portic-devops -u ${USER} -p ${PASS}
 echo "PUSH default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:${BUILD_NUMBER}"
 docker push default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:${BUILD_NUMBER}
+docker push default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:latest
+
 echo "SET IMAGE"
-oc set image --loglevel=1  deployment/tomcat8-portic tomcat8-portic=image-registry.openshift-image-registry.svc:5000/portic-devops/${IMAGE}:${BUILD_NUMBER}
-echo "c set image --loglevel=1  deployment/tomcat8-portic tomcat8-portic=image-registry.openshift-image-registry.svc:5000/portic-devops/${IMAGE}:${BUILD_NUMBER}"
-#echo "TAG LATEST"
-#docker tag default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:${BUILD_NUMBER} default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:latest
-#echo "PUSH LATEST"
-#docker push default-route-openshift-image-registry.apps-crc.testing/portic-devops/${IMAGE}:latest
+oc set image --loglevel=2  deployment/tomcat8-portic tomcat8-portic=image-registry.openshift-image-registry.svc:5000/portic-devops/${IMAGE}:${BUILD_NUMBER}
 
 oc delete configmap tomcat-serverxml --ignore-not-found=true
 oc delete configmap tomcat-prometheus-config --ignore-not-found=true
